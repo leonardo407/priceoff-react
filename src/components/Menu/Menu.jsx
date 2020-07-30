fd65aaffe33946fa199e590bd420ae28ffd32c6a
Leonid <leo1133407@list.ru>
2020-07-30 16:47:38 +0300

    added based styles

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Menu.module.scss'
+
+export const Menu = () => (
+  <header className={classes.Menu}>
+    <div className='container'>
+      <h2 className="logo">ЦенOff</h2>
+    </div>
+  </header>
+)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
